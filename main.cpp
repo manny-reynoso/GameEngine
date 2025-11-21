9037155500a91c19cda2057acf06a3e7e137084b
manny-reynoso <manny.d.reynoso@gmail.com>
2025-11-21 14:51:44 -0600

    Seventh commit from timeline.

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ class Player {
         int baseDamage = attackMechanic();        // get base damage
         int finalDamage = critAttack(baseDamage); // apply crit if any
 
-        cout << "Player hit for " << damageTaken << "!" << endl;
+        cout << "Player hit for " << "test" << "!" << endl;
         return finalDamage;
     };
 
