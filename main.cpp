b163156483c68f04a4af1b61d3d844a9079afaf4
manny-reynoso <manny.d.reynoso@gmail.com>
2025-12-02 13:53:02 -0600

    27th commit from timeline.

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ class Player {
         
         int damageTaken = static_cast<int>(finalDamage*((armour / 100.0)-100.0));
         cout << name << " has an armour rating of "  << armour << endl;
-        cout << "Player got hit for " << damageTaken << endl;
+        cout << name <<" got hit for " << damageTaken << endl;
 
         return damageTaken;
     }
